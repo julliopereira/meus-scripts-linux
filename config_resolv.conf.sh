ed420bdd5742d5e4e3e41fd715f0ec87250bf1a4
Julio Cesar Pereira <julliopereira@gmail.com>
2022-05-17 15:08:33 -0300

    atualizado resolv

--- config_resolv.conf.sh
+++ config_resolv.conf.sh
@@ -5,11 +5,9 @@ domain home.com
 nameserver 10.0.2.105
 nameserver 8.8.8.8
 "
-
-
 resolv () {
-	sed -i 's/^[a-z]/\#/g' resolv.conf
-	echo $resolv >> /etc/resolv.conf
+	> /etc/resolv.conf
+	echo "$resolv" >> /etc/resolv.conf
 }
 
 resolv
